a10c7e83b0cfa861bb4e40f25b6e79894af9d2b3
umer <umer4167@hotmail.com>
2019-09-11 19:58:27 +0300

    file size check added

--- src/components/ImageUpload2.js
+++ src/components/ImageUpload2.js
@@ -6,19 +6,42 @@ const CLOUD_NAME = "drakuseth";
 const API_KEY = "181494391349546";
 
 export default function ImageUpload2() {
+  let error = {};
   const [selectedFile, setState] = useState(null);
+  const [ImageUrl, setImageUrl] = useState("");
+  const [fileSizeError, setError] = useState("");
 
   const getImage = event => {
-    setState({ ...selectedFile, selectedFile: event.target.files[0] });
+    if (checkFileSize(event)) {
+      setState({ ...selectedFile, selectedFile: event.target.files[0] });
+    }
+    setError(error.sizeError);
   };
+
+  const checkFileSize = event => {
+    let file = event.target.files[0];
+    let fileSize = file.size / 1024 / 1024;
+    let size = 3;
+    console.log(fileSize);
+    if (fileSize > size) {
+      error.sizeError = "File Size too big. Pick smaller size";
+    }
+    if (Object.keys(error).length > 0) {
+      event.target.value = "";
+      return false;
+    }
+    return true;
+  };
+
   const uploadImage = e => {
     e.preventDefault();
+    //validate
     const file = selectedFile.selectedFile;
     const formData = new FormData();
-    formData.append("file", file);
-    formData.append("api_key", API_KEY);
-    formData.append("upload_preset", PRESET_NAME);
-    formData.append("timestamp", (Date.now() / 1000) | 0);
+    // formData.append("file", file);
+    // formData.append("api_key", API_KEY);
+    // formData.append("upload_preset", PRESET_NAME);
+    // formData.append("timestamp", (Date.now() / 1000) | 0);
     return axios
       .post(
         `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`,
@@ -30,7 +53,7 @@ export default function ImageUpload2() {
       .then(response => {
         const data = response.data;
         const fileURL = data.secure_url; // You should store this URL for future references in your app
-        console.log(data);
+        setImageUrl(fileURL);
       });
   };
   return (
@@ -39,6 +62,10 @@ export default function ImageUpload2() {
 
       <input type="file" onChange={getImage} />
       <button onClick={uploadImage}>Upload Image</button>
+      <div>
+        <img src={ImageUrl} alt="" />
+      </div>
+      <h1>{fileSizeError}</h1>
     </div>
   );
 }
